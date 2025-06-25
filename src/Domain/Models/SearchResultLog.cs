19677aa453de5a0c88884a357ae5b1e110e38ff7
立入 <88256484+i-tachiiri@users.noreply.github.com>
2025-06-25 18:18:10 +0900

    Add Newtonsoft.Json to Domain (#41)

--- src/Domain/Models/SearchResultLog.cs
+++ src/Domain/Models/SearchResultLog.cs
@@ -1,12 +1,33 @@
 using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Domain.Models;
 
 public record SearchResultLog(
-    [property: JsonPropertyName("id")] string Id,
-    [property: JsonPropertyName("session_id")] string SessionId,
-    [property: JsonPropertyName("place_id")] string PlaceId,
-    [property: JsonPropertyName("query")] string Query,
-    [property: JsonPropertyName("lat")] decimal Lat,
-    [property: JsonPropertyName("lng")] decimal Lng,
-    [property: JsonPropertyName("searched_at")] DateTime SearchedAt);
+    [property: JsonPropertyName("id")]
+    [property: JsonProperty("id")]
+    string Id,
+
+    [property: JsonPropertyName("session_id")]
+    [property: JsonProperty("session_id")]
+    string SessionId,
+
+    [property: JsonPropertyName("place_id")]
+    [property: JsonProperty("place_id")]
+    string PlaceId,
+
+    [property: JsonPropertyName("query")]
+    [property: JsonProperty("query")]
+    string Query,
+
+    [property: JsonPropertyName("lat")]
+    [property: JsonProperty("lat")]
+    decimal Lat,
+
+    [property: JsonPropertyName("lng")]
+    [property: JsonProperty("lng")]
+    decimal Lng,
+
+    [property: JsonPropertyName("searched_at")]
+    [property: JsonProperty("searched_at")]
+    DateTime SearchedAt);
